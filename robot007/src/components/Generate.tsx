924c46eb0f93a644b4e0b7633a474b92e0c8572e
Mootassam <mootassame@gmail.com>
2026-03-01 15:17:51 -0800

    add the new PORT of backend

--- robot007/src/components/Generate.tsx
+++ robot007/src/components/Generate.tsx
@@ -361,7 +361,7 @@ function WhatsAppNumberGenerator() {
 
     try {
       // Create socket with better error handling and reconnection settings
-      newSocket = io("http://159.198.47.173:8088", {
+      newSocket = io("http://159.198.47.173:8087", {
         reconnectionAttempts: 5,
         reconnectionDelay: 1000,
         reconnectionDelayMax: 5000,
